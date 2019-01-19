a53ee588606dd5ccfa27b21f3e5184bb445a2bd1
Akritas1970 <akritas1970@gmail.com>
2019-01-19 12:57:20 -0500

    Pseudocode

---
+++ greatBay.js
@@ -0,0 +1,16 @@
+// Require inquire module
+
+//Create a  connection through mysql
+//Connect to host --Run prompt function here
+
+//Start of Prompt function - Use Inquire
+//Question 1: "Post or Bid on an Item"
+//If post then ask "what item would you like to submit" - then "what category" and "what would you like the starting bid to be?
+//If Bid then ask user "What item to bid on" - and "how much to bid on item"
+//Query database for item
+
+//Post-bid function -- Create row product table
+
+//Bid-on item function -- Update row user table
+
+//Read (comparison) of tables function
